7857a894bc40e20fdde17c41f06290da5dd5dd1d
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2023-09-18 11:37:35 -0600

    Second commit

--- print_reqs.py
+++ print_reqs.py
@@ -3,3 +3,6 @@ import requests
 print(requests.__version__)
 
 requests.get('http://google.com')
+
+print(requests.get(
+    'https://raw.githubusercontent.com/asvpglory/404Lab1/main/print_reqs.py').content)
